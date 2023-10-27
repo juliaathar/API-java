ab0942a132745caecc707184f85743f7a6b8ba55
juliaathar <47447272809@info.local>
2023-10-26 21:53:57 -0300

    feito método de deletar

--- src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -53,7 +53,7 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
     }
 
-    @PutMapping
+    @PutMapping("/{idUsuario}")
     public ResponseEntity<Object> editarUsuario(@PathVariable(value = "idUsuario") UUID id, @RequestBody @Valid UsuarioDto usuarioDto){
         Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
 
@@ -61,10 +61,23 @@ public class UsuarioController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
         }
 
-        UsuarioModel usuario = new UsuarioModel();
+        UsuarioModel usuario = usuarioBuscado.get();
         BeanUtils.copyProperties(usuarioDto, usuario);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @DeleteMapping("/{idUsuario}")
+    public ResponseEntity<Object> deletarUsuario(@PathVariable(value = "idUsuario") UUID id){
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if(usuarioBuscado.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
+        }
+
+        usuarioRepository.delete(usuarioBuscado.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Usuário deletado!");
     }
 
 }
